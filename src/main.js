07ae2910c698857d2c7451e4112078ec33a543aa
kai-1208 <tkhski1208@gmail.com>
2024-10-25 23:34:38 +0900

    update src/index.html, main.js and styles.css

--- src/main.js
+++ src/main.js
@@ -8,53 +8,61 @@ document.addEventListener("DOMContentLoaded", () => {
     // const contentList = [];
     const button1 = document.getElementById("button1");
     button1.addEventListener("click", function() {
-        i++;
-        contentLists[i] = [];
         const text1 = document.getElementById("content1");
         const text2 = document.getElementById("content2");
         let message1 = text1.value.replace(/[\s\u3000]/g, "");
         let message2 = text2.value.replace(/[\s\u3000]/g, "");
-        // console.log(message1);
-        // console.log(message2);
-        // if (message1.trim() != "" && message2.trim() != "") {
-        //     contentList.push(message1);
-        //     contentList.push(message2);
-        //     console.log(contentList);
-        // }
-        contentLists[i].push(message1);
-        contentLists[i].push(message2);
-        console.log(i + ":"+ contentLists[i]);
 
-        text1.value = "";
-        text2.value = "";
+        if (message1 != "" && message2 != "") {
+            i++;
+            contentLists[i] = [];
+            // const text1 = document.getElementById("content1");
+            // const text2 = document.getElementById("content2");
+            // let message1 = text1.value.replace(/[\s\u3000]/g, "");
+            // let message2 = text2.value.replace(/[\s\u3000]/g, "");
 
-        // const resultDiv = document.getElementById("result");
+            // console.log(message1);
+            // console.log(message2);
+            // if (message1.trim() != "" && message2.trim() != "") {
+            //     contentList.push(message1);
+            //     contentList.push(message2);
+            //     console.log(contentList);
+            // }
+            contentLists[i].push(message1);
+            contentLists[i].push(message2);
+            console.log(i + ":"+ contentLists[i]);
 
-        // const newContent = document.createElement('div');
-        // newContent.id = "id" + i
-        // const content1 = contentLists[i][0];
-        // const content2 = contentLists[i][1];
-        // newContent.textContent = `単語: ${content1}, 意味: ${content2}`;
-        // resultDiv.appendChild(newContent);
+            text1.value = "";
+            text2.value = "";
 
-        sendDataToServer(contentLists)
-        .then(data => {
-            if (data.status === "success") {
-                // console.log(data);
-                const resultDiv = document.getElementById("result");
+            const resultDiv = document.getElementById("result");
 
-                const newContent = document.createElement('div');
-                newContent.id = "id" + i;
-                const content1 = contentLists[i][0];
-                const content2 = contentLists[i][1];
-                newContent.textContent = `単語: ${content1}, 意味: ${content2}`;
-                resultDiv.appendChild(newContent);
-            }else{
-                // console.error(data);
-            }
-        });
-        document.addEventListener("click", () => {
-            
-        });
+            const newContent = document.createElement('div');
+            newContent.id = "id" + i
+            const content1 = contentLists[i][0];
+            const content2 = contentLists[i][1];
+            newContent.textContent = `単語: ${content1}, 意味: ${content2}`;
+            resultDiv.appendChild(newContent);
+
+            sendDataToServer(contentLists)
+            .then(data => {
+                if (data.status === "success") {
+                    // console.log(data);
+                    const resultDiv = document.getElementById("result");
+
+                    const newContent = document.createElement('div');
+                    newContent.id = "id" + i;
+                    const content1 = contentLists[i][0];
+                    const content2 = contentLists[i][1];
+                    newContent.textContent = `単語: ${content1}, 意味: ${content2}`;
+                    resultDiv.appendChild(newContent);
+                }else{
+                    // console.error(data);
+                }
+            });
+            document.addEventListener("click", () => {
+                
+            });
+        }
     });
 });
